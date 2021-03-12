7a0e182ea37cab13d5f24c15fce4f6aea9f60964
Christian Gallo <42328477+Gallo9923@users.noreply.github.com>
2021-03-12 09:43:23 -0500

    Added data creation (#1)

---
+++ complexity-experiment/data-creation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace data_creation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            saveFile(49152); // 49152 - 1.5 MB
+            saveFile(327680); //  327,680 - 10 MB
+            saveFile(491520); // 491,520 -  15 MB 
+        }
+
+        private static void saveFile(int N)
+        {
+            string fileName = "../../data/" +N + ".txt";
+            FileStream fs = null;
+            
+            try
+            {
+                fs = new FileStream(fileName, FileMode.CreateNew);
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    string[] lines = new string[N];
+                    Random r = new Random();
+
+                    for (int i = 0; i < N; i++)
+                    {
+                        writer.WriteLine(r.Next(Int32.MinValue, Int32.MaxValue).ToString());
+                    }
+                }
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Dispose();
+            }
+        }
+        
+    }
+}
